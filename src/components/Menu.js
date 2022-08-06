300117efd2c7ac10fb33171818276e1fc49555fb
ThiagoCCR <thiago.ccr96@gmail.com>
2022-08-06 12:54:47 -0300

    feat progressbar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,19 +1,36 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 import styled from "styled-components";
-
-
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import UserContext from "../contexts/UserContext";
 
 export default function Menu() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { progress, setProgress } = useContext(UserContext);
 
   return (
     <Wrapper>
-      <button onClick={()=>navigate("/habitos")}>Hábitos</button>
-      <button onClick={()=>navigate("/hoje")}>
-        <img alt="Menu" src="../assets/img/menu.png" />
-      </button>
-      <button onClick={()=>navigate("/historico")}>Histórico</button>
+      <button onClick={() => navigate("/habitos")}>Hábitos</button>
+      <ProgressBar onClick={() => navigate("/hoje")}>
+        <CircularProgressbar
+          value={progress}
+          text={`Menu`}
+          background
+          backgroundPadding={6}
+          styles={buildStyles({
+            backgroundColor: "#3e98c7",
+            textColor: "#fff",
+            pathColor: "#fff",
+            trailColor: "transparent",
+          })}
+        />{""}
+      </ProgressBar>
+      <button onClick={() => navigate("/historico")}>Histórico</button>
     </Wrapper>
   );
 }
@@ -38,4 +55,12 @@ const Wrapper = styled.div`
     border: none;
     color: #52b6ff;
   }
+`;
+
+const ProgressBar = styled.button`
+  height: 91px;
+  width: 91px;
+  background-color: #52b6ff;
+  border-radius: 50%;
+  margin-bottom: 20px;
 `;
